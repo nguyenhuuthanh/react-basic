b19041d6164bfa1ac9c086a0fe63471d8bd00e16
Thanh Nguyen <huuthanhsv@gmail.com>
2020-08-28 10:51:09 +0700

    Handlind Event: setState to update local state

--- src/components/ComponentState.js
+++ src/components/ComponentState.js
@@ -1,8 +1,38 @@
 import React from 'react';
 
 class ComponentState extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      name: 'ReactJS',
+    };
+
+    // This binding is necessary to make `this` work in the callback
+    // https://reactjs.org/docs/handling-events.html
+    this.handleChangeName = this.handleChangeName.bind(this);
+  }
+
+  /**
+   * We use setState to update local state
+   */
+  handleChangeName(e) {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
   render() {
-    return <h1>Hello, Components and Props</h1>
+    return (
+      <section>
+        <h1>Hello, Components and Props</h1>
+        <h2>Now it is {this.state.date.toLocaleDateString()}.</h2>
+        <input
+          value = {this.state.name}
+          onChange = {this.handleChangeName}
+        />
+      </section>
+    );
   }
 }
 
