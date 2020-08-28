c202ba324f27c9f09e89f96dcf92f251c047aafa
ThanhNguyen <thanh.nguyen@saritasa.com>
2020-08-28 10:23:47 +0700

    Fist commit

---
+++ src/components/ComponentState.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ComponentState extends React.Component {
+  render() {
+    return <h1>Hello, Components and Props</h1>
+  }
+}
+
+export default ComponentState;
---
+++ src/components/Hooks.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Hooks extends React.Component {
+  render() {
+    return <h1>Hello, Hooks</h1>
+  }
+}
+
+export default Hooks;
